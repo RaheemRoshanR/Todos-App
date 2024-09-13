fb5e38e59082419b6209571f567ded9ae5d59ef1
RaheemRoshanR <120947563+RaheemRoshanR@users.noreply.github.com>
2024-09-13 15:26:12 +0500

    Add files via upload

---
+++ src/features/todo/TodoSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "Hello World",
+    },
+  ],
+};
+
+export const todosSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo } = todosSlice.actions;
+export const todoReducer = todosSlice.reducer;
